5cb08c855e391344cea618fcb124de034c258ebe
Oleg Lipin <gesundes@gmail.com>
2022-07-20 17:44:11 +0300

    Update dag.py

--- dag.py
+++ dag.py
@@ -1,24 +1,83 @@
-from datetime import datetime
-import os
 
-from airflow.models import DAG
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime, timedelta
+from textwrap import dedent
 
-print(os.environ["AIRFLOW_HOME"])
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
 
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
 with DAG(
-        dag_id='example_spark_operator_sdp',
-        schedule_interval=None,
-        start_date=datetime(2021, 1, 17),
-        catchup=False,
-        tags=['sdp_spark'],
+    'tutorial',
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function,
+        # 'on_success_callback': some_other_function,
+        # 'on_retry_callback': another_function,
+        # 'sla_miss_callback': yet_another_function,
+        # 'trigger_rule': 'all_success'
+    },
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=['example'],
 ) as dag:
-    # [START howto_operator_spark_submit]
-    submit_job = SparkSubmitOperator(
-        application="dags/repo/tests/test_spark/pi.py",
-        application_args=["10"],
-        task_id="submit_job",
-        conn_id="data_platform_spark",
-        conf={"spark.pyspark.python": "/opt/anaconda3/bin/python3"}
+
+    # t1, t2 and t3 are examples of tasks created by instantiating operators
+    t1 = BashOperator(
+        task_id='print_date',
+        bash_command='date',
     )
-    # [END howto_operator_spark_submit]
+
+    t2 = BashOperator(
+        task_id='sleep',
+        depends_on_past=False,
+        bash_command='sleep 5',
+        retries=3,
+    )
+    t1.doc_md = dedent(
+        """\
+    #### Task Documentation
+    You can document your task using the attributes `doc_md` (markdown),
+    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+    rendered in the UI's Task Instance Details page.
+    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+
+    """
+    )
+
+    dag.doc_md = __doc__  # providing that you have a docstring at the beginning of the DAG
+    dag.doc_md = """
+    This is a documentation placed anywhere
+    """  # otherwise, type it like this
+    templated_command = dedent(
+        """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ macros.ds_add(ds, 7)}}"
+    {% endfor %}
+    """
+    )
+
+    t3 = BashOperator(
+        task_id='templated',
+        depends_on_past=False,
+        bash_command=templated_command,
+    )
+
+    t1 >> [t2, t3]
